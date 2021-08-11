096b196394b38f2c7e8c10ba85610d798379104e
David Darx <daviddarx@gmail.com>
2021-08-11 21:05:19 +0200

    feat(home) implement content structure for anchor links

--- src/components/pages/home.vue
+++ src/components/pages/home.vue
@@ -12,6 +12,27 @@
       v-html="getHTMLfromMD(home.content)"
     >
     </div>
+    <div id="intro-p1">
+      intro 1
+    </div>
+    <div id="intro-p2">
+      intro 2
+    </div>
+    <div id="about">
+      <h2>
+        Über mich
+      </h2>
+    </div>
+    <div id="availablity">
+      <h2>
+        Verfügbarkeit
+      </h2>
+    </div>
+    <div id="contact">
+      <h2>
+        Kontakt
+      </h2>
+    </div>
   </div>
 </template>
 
