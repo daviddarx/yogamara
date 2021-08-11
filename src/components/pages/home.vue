9fc195084c743cb59b73f0cbedee284fbab5b84b
David Darx <daviddarx@gmail.com>
2021-08-11 20:57:58 +0200

    feat(home) removes footer

--- src/components/pages/home.vue
+++ src/components/pages/home.vue
@@ -12,20 +12,16 @@
       v-html="getHTMLfromMD(home.content)"
     >
     </div>
-
-    <custom-footer></custom-footer>
   </div>
 </template>
 
 <script>
   import Vue from "vue";
-  import CustomFooter from '../footer.vue';
   import contents from '../../contents';
   import getHTMLfromMDMixin from '../../mixins/getHTMLFromMD';
 
   export default Vue.extend({
     components: {
-      'custom-footer': CustomFooter,
     },
     mixins: [getHTMLfromMDMixin],
     data() {
